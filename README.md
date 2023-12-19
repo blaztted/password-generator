b2af75d93ff9f17abe7915196fcbd361ae628135
blaztted <bbc.silva@gmail.com>
2023-12-19 22:13:57 +0000

    Finished REAME file with image of the final result

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# password-generator
+# Password-Generator Project
+
+## The Repository
+
+This repository is where I code my [Password-Generator](https://github.com/blaztted/password-generator) project in order to showcase some of my javascript skills to potential employers or clients.
+
+## Deployed Site
+
+<p align="center">
+  <img alt="frontpage and console print" src="./assets/images/password-gen.png">
+
+You can find the final result [here](https://blaztted.github.io/password-generator/).
+
+</p>
+
+## Links
+
+https://github.com/blaztted/password-generator
+
+https://blaztted.github.io/password-generator/
+
+## License
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Licensed under the [MIT](LICENSE.txt) license.
--- script.js
+++ script.js
@@ -97,7 +97,6 @@ function getPasswordOptions() {
     return getPasswordOptions();
   }
 
-  // Ask which character sets to use
   const includeUppercase = confirm("Include uppercase letters?");
   const includeLowercase = confirm("Include lowercase letters?");
   const includeNumbers = confirm("Include numbers?");
@@ -129,7 +128,6 @@ function getPasswordOptions() {
     alert("Error: No character set selected.");
     return "";
   }
-
   // Return an object with user-selected options
   return {
     passwordLength,
@@ -178,46 +176,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// const charOptions = [];
-// const generatedPassword = "";
-// You can store the generatedPassword as a string and concat each character OR
-// as an array and push each character, then join once you have enough characters
-
-//function getPasswordOptions() {
-// Prompt for password len32gth
-// At least 8 characters, no more than 128 characters
-// Conditional to check that the number that was entered is in range
-// Prompts store data as strings, so need to parse into a number
-// If the user's input is out of range, either return out of the function or call the function again
-
-// Confirm which character sets to use
-// If the user answers false for all, either return out of the function or call the function again
-
-// Once they select a character set:
-// Generate a random character for each selected character set
-// Either push selected character sets to a mega-array of all selected characters
-// OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-
-// Once character sets are selected, move on to generating random characters
-//}
-
-// Function for getting a random element from an array
-//function getRandom(arr) {
-// Need a variable to hold the password as it's being generated
-// Need a variable to hold the index that's being generated
-
-// For loop that loops the number of times that matches the length the user chose
-// Generate a random number
-// That number is the index for a character in the mega-array
-// So then, mega-array[generated-index] is the actual character
-// Add that character to the password
-
-// Once we finish the for loop, return the generated password
-//}
-
-// Function to prompt user for password options
-//function getPasswordOptions() {}
-
-// Function for getting a random element from an array
-//function getRandom(arr) {}
